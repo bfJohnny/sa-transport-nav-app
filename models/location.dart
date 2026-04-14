799193fdc1aa4b23096d2572d096149b3de0667a
bfJohnny <50967054+bfJohnny@users.noreply.github.com>
2026-04-14 03:37:05 +0200

    Create Location model class with latitude, longitude, address, and accuracy fields

---
+++ models/location.dart
@@ -0,0 +1,8 @@
+class Location {
+  final double latitude;
+  final double longitude;
+  final String address;
+  final double accuracy;
+
+  Location({required this.latitude, required this.longitude, required this.address, required this.accuracy});
+}
